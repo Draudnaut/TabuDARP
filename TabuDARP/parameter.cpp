f847c814a2549c58a7883602601c6a8118da0713
henhimicons <kanberra354562007@gmail.com>
2019-02-26 13:57:20 +0800

    paratemer class

--- TabuDARP/parameter.cpp
+++ TabuDARP/parameter.cpp
@@ -10,3 +10,51 @@ Parameter::Parameter(double _alpha, double _beta, double _gamma, double _tao, do
 	delta = _delta;
 	eta = _eta;
 }
+
+double Parameter::get_alpha()
+{
+	return alpha;
+}
+
+double Parameter::get_beta()
+{
+	return beta;
+}
+
+double Parameter::get_gamma()
+{
+	return gamma;
+}
+
+double Parameter::get_tao()
+{
+	return tao;
+}
+
+double Parameter::get_delta()
+{
+	return delta;
+}
+
+double Parameter::get_eta()
+{
+	return eta;
+}
+
+void Parameter::update(bool feasible)
+{
+	if (feasible)
+	{
+		alpha /= (1 + delta);
+		beta /= (1 + delta);
+		gamma /= (1 + delta);
+		tao /= (1 + delta);
+	}
+	else
+	{
+		alpha *= (1 + delta);
+		beta *= (1 + delta);
+		gamma *= (1 + delta);
+		tao *= (1 + delta);
+	}
+}
--- TabuDARP/parameter.h
+++ TabuDARP/parameter.h
@@ -5,4 +5,11 @@ private:
 	double alpha, beta, gamma, tao, delta,eta;
 public:
 	Parameter(double _alpha, double _beta, double _gamma, double _tao, double _delta, double _eta);
+	double get_alpha();
+	double get_beta();
+	double get_gamma();
+	double get_tao();
+	double get_delta();
+	double get_eta();
+	void update(bool feasible);
 };
