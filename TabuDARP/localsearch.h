3855d256b979ec108b26e685d377e5ffc331d71b
henhimicons <kanberra354562007@outlook.com>
2019-04-20 15:01:59 +0800

    local search fixed

--- TabuDARP/localsearch.h
+++ TabuDARP/localsearch.h
@@ -25,19 +25,6 @@ struct neighbor_structure
 	int tour2;
 	int request;
 };
-struct vnsSwapNeighborhoodStructure
-{
-	int tour1;
-	std::vector<int> requestTour1Selected;
-	int tour2;
-	std::vector<int> requestTour2Selected;
-};
-struct vnsChainNeighborhoodStructure
-{
-	int tour1;
-	int tour2;
-	std::vector<int> requestSeq;
-};
 std::vector<neighbor_structure> getNeighbors(solution s, Data &d, int iteration, Record_move &rm, Parameter &p);
 std::vector<neighbor_structure> getNeighbors_ex(solution s,Data &d,int iteration,Record_move &rm,Parameter &p);
 solution BigRemove(solution s,Data &d,Parameter &p);
@@ -49,6 +36,5 @@ solution present_solution(solution Base, neighbor_structure change, Parameter &p
 void paraTabuSearch(solution s, Parameter p, Data &d, Record_move &rm);
 solution getSwapNeighbor(solution s);
 solution getSwapNeighbor_ex(solution s);
-solution presentSwapSolution(solution Base, vnsSwapNeighborhoodStructure neighbor_present, Parameter &p, Data &d);
 solution getChainNeighbor(solution s);
 solution getChainNeighbor_ex(solution s);
